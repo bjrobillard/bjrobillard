ddbd9c0ac5be7321d25469507062627030f24661
Benjamin Robillard <bjrobillard@mavs.coloradomesa.edu>
2022-05-03 18:00:52 -0600

    started work on final proj

---
+++ src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title(){
+    return (
+        <>
+            <h1 data-testid='titleHeaderTest' className='titleHeader'> The Magic Square </h1> 
+        </>
+    )
+}
+
+export default Title;
