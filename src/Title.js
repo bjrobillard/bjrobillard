d8e7ab8d7b305d4435fc717a35f2b002c9fceec2
Benjamin Robillard <bjrobillard@mavs.coloradomesa.edu>
2022-05-12 17:10:38 -0600

    i dont even know

--- src/Title.js
+++ src/Title.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Title(){
     return (
-        <>
+        <div className='title'>
             <h1 data-testid='titleHeaderTest' className='titleHeader'> The Magic Square </h1> 
-        </>
+        </div>
     )
 }
 
